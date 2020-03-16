ad7db2b17a7817a53b7fd7a19ef670cdc36ec404
Steph Walsh <stephanienicolewalsh@gmail.com>
2020-03-16 17:23:45 -0600

    updated file names and navigation

---
+++ 2018.php
@@ -0,0 +1,30 @@
+<?php include "functions.php"; ?>
+<?php include "includes/header.php";?>
+
+	<section class="content">
+
+	<aside class="col-xs-4">
+
+	<?php Navigation();?>
+			
+			
+	</aside><!--SIDEBAR-->
+
+
+<article class="main-content col-xs-8">
+		
+
+
+		<?php  
+
+        //This function says hello!
+        echo "<h1> Hello </h1>";
+
+
+		?>
+
+	
+
+		</article><!--MAIN CONTENT-->
+
+<?php include "includes/footer.php"; ?>
